a6ae2623d7c391300acd77eaa947627a95c32afd
MrSloth13 <50780834+MrSloth13@users.noreply.github.com>
2020-01-06 22:46:09 -0500

    added the next section of notes. onto the numbers.

---
+++ Numbers.py
@@ -0,0 +1,5 @@
+#Overview of Number Types in Python--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title1 = 'Overview of Number Types in Pythons'
+section_spacer = f'{section_title1}--------------------------------------------'
+print(section_spacer)
