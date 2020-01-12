85fd1379b7dc596f6eac801bc79c769b9700cd5c
MrSloth13 <50780834+MrSloth13@users.noreply.github.com>
2020-01-12 03:53:51 -0500

    just updating for the night

---
+++ Collections.py
@@ -0,0 +1,19 @@
+section_title1 = 'Section introduction'
+section_space1 = f'{section_title1}--------------------------------------------'
+print(section_space1)
+
+#notes
+"""
+So in the last two sections we covered strings and list but in this one we will be getting a bit more abstract with collections. now a collection isnt really a data type is a "collection"
+of different or the same data type. in this section we will be going over three data types with list, libararies, and tuples. i think tuples is the one i know the least about at the current
+momnet. now a list fucntions a ton like an array from JavaScript or C#. a list or collection of various variables or classes or what ever you want really. now a dictionary or libarary works
+with keys. where something has a key which has a corrilating value to it. that could be a funciton or an operator and just a normal variable. now a tuple, from what he has said so far, is 
+strucuted much like the list. he didn't go into detail on how its different but says it more like the list rather then the dictonary. 
+"""
+
+#Overview of Lists in Python-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title2 = 'Overview of Lists in Python'
+section_space2 = f'{section_title2}--------------------------------------------'
+print(section_space2)
+
