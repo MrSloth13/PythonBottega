8a0fafa14448ff47ac86f7cf0d87ef3a5050af04
MrSloth13 <50780834+MrSloth13@users.noreply.github.com>
2020-01-16 17:45:04 -0500

    Finished the first section of git

--- GitOverview.py
+++ GitOverview.py
@@ -1,3 +1,29 @@
 section_title1 = 'Git Overview'
 section_space1 = f'{section_title1}--------------------------------------------'
 print(section_space1)
+
+"""
+What is git?
+
+Git is a free and open source distributed version control system.
+
+-Open source:
+Free to use and developed by a community of developers.
+
+-Distributed:
+Centralized code repository.
+:: This is used for either a good backup or one of the best ways to develope code within a team. since you both can start with the same source file but you can
+branch off it and it will not effect the original file. 
+
+-Version Control System:
+Gives teh ability to create milestones for a project.
+:: so in a nut shell you can branch off a "version" of the project into a new branch and make it your new main branch in the feature set and stability make sense
+to do so. Now this means that you can always go back. all versions are saved in git. so if you make a new branch and then when you put in back in and everything 
+breaks, you can go back to the stable version and try again. 
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\
+
+section_title2 = ''
+section_space2 = f'{section_title2}--------------------------------------------'
+print(section_space2)
