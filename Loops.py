b8f9e5aa64a8898cfafa82c3189b4eaf2f1ec2cd
JellyHead <JellyHeadM@outlook.com>
2020-02-07 23:12:28 -0500

    Finihsed the introduction to loops in python.

--- Loops.py
+++ Loops.py
@@ -4,7 +4,16 @@ print(section_space1)
 
 #i am now out of the data stuctures!!! now onto loops
 
-
+#notes
+"""
+So there are two types of list in python. there is a 'for in' loop and a 'while' loop. a for in loop will run through as many items as you have. simple and will be use about 90% of the time. the 
+while loop is similar to C# in where if you dont give it an exit point or a point to end it will just go on and crash the system with an infinite loop. but just like it C# the while loop can still 
+be useful. especially if you dont want to go through an enitre list to save on resources. 
+"""
 
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title2 = "How to Implement Python Loops for Lists, Tuples, and Dictionaries"
+section_space2 = f'{section_title2}--------------------------------------------'
+print(section_space2)
