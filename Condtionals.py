43232fb60ad32e37029916e0be2316843e5f0b60
JellyHead <JellyHeadM@outlook.com>
2020-02-15 08:21:55 -0500

    just getting the files ready for the new section.

---
+++ Condtionals.py
@@ -0,0 +1,6 @@
+section_title1 = "Introduction to Python Conditionals"
+section_space1 = f'{section_title1}--------------------------------------------'
+print(section_space1)
+
+#we are onto condtionals now!! whooooo!!!!
+
