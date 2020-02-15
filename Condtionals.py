992b52c2f328cdb62824765988c1f666a084d306
JellyHead <JellyHeadM@outlook.com>
2020-02-15 09:47:01 -0500

    finished a section on the ternary operator in python conditionals

--- Condtionals.py
+++ Condtionals.py
@@ -35,3 +35,28 @@ close it off with : unlike the else where you put the : right after else. it a g
 section_title3 = 'How to Use the Ternary Operator in Python Conditionals'
 section_space3 = f'{section_title3}--------------------------------------------'
 print(section_space3)
+
+role = 'admin'
+
+auth = 'can access' if role == 'admin' else 'cannot access'
+
+print(auth)
+
+passoword_for_pillow_fort = 'peanuts'
+
+whats_the_password = 'That is the password, come in' if passoword_for_pillow_fort == 'peanuts' else 'You are not allowed'
+
+print(whats_the_password)
+
+#notes
+"""
+Okay so this is sweet but jordan did offer some caution when making these since they do add complexity and can make it harder to read but definalty have there place. So you would use this when you want to assign things
+with a conditional. and all it really does is remaps a normal if else conditional. you would name the variable and assign it (=), then put what you want to happen if the condition is true, then put your if statement
+like if 2 == 2, then you put else, then what you would want to happen if the conditional is not met. simple but i can see how it can be hard to read. 
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title4 = 'Full List of Python Conditional Operators'
+section_space4 = f'{section_title4}--------------------------------------------'
+print(section_space4)
