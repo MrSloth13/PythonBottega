cd59960ada5b956711dcc7d271dac394650fd88f
JellyHead <JellyHeadM@outlook.com>
2020-02-15 08:44:33 -0500

    finished a section going over conditionals at a very high level view.

--- Condtionals.py
+++ Condtionals.py
@@ -4,3 +4,34 @@ print(section_space1)
 
 #we are onto condtionals now!! whooooo!!!!
 
+#notes
+"""
+this is one of the most important things in programming. all it really is, is a way to check to see if a senario is true, then exicute specific code for it. 
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title2 = 'Overview of Python Conditionals'
+section_space2 = f'{section_title2}--------------------------------------------'
+print(section_space2)
+
+age = 25
+
+if age == 25:
+    print('Supeeerrr')
+elif age > 100:
+    print('you too old my guy')
+else:
+    print('noooppe')
+
+#notes
+"""
+so this was mostly just review for me but i did not know about the else if conditional. so you spell it like elif for else if and then you put a normal conditional check syntax then 
+close it off with : unlike the else where you put the : right after else. it a good way to add another conditional or a nested on after a first conditional. pretty cool.
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title3 = 'How to Use the Ternary Operator in Python Conditionals'
+section_space3 = f'{section_title3}--------------------------------------------'
+print(section_space3)
