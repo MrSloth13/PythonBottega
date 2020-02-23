50377d59a9338927450932a8c2e48a493bbd771e
JellyHead <JellyHeadM@outlook.com>
2020-02-23 12:22:23 -0500

    Finished the introduction to the new section on importin gmodules in python.

--- ImportingModules.py
+++ ImportingModules.py
@@ -2,4 +2,20 @@ section_title1 = 'Python Importing Module Section Introduction'
 section_space1 = f'{section_title1}--------------------------------------------'
 print(section_space1)
 
+#since this is an introduction it will mostly be just notes. 
+"""
+So we have done this a tad bit in some of the projects but now it gets its own section in the course. which is well needed. so importing a module is really just importing a library from the 
+commmunity that has its own functions built in and ready to use to free up some of your time. you still can build everything out if you wanted to but sometimes its just smarter to use what is
+already made instead of constantly trying to remake the wheel. 
+
+Alright so there was a bit more to this then i thought but i was still a bit right. so there are some libaraies like the math one that are still in the main python libarary. you still need to 
+import them but don't need to install them. now there are some that have to be installed which i have already done as well though. you need to make a connection to the libarary which is where
+PIP comes in. finally i see why this is use. when you do a pip install it is importing the libarary onto the system, and then you can import them into your project like the math function and 
+other core python libararies. so i have know about this all along but haven't known why i was doing it. 
+"""
+
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title2 = 'How to Create a Custom Module and Import It In the Python Repl'
+section_space2 = f'{section_title2}--------------------------------------------'
+print(section_space2)
