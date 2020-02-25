c02530286e9ed7a3851b05649aa9e0d8e19c7259
JellyHead <JellyHeadM@outlook.com>
2020-02-24 22:24:37 -0500

    finished a section on making your own modules and importing them in. so fucking cool lol.

--- ImportingModules.py
+++ ImportingModules.py
@@ -19,3 +19,22 @@ other core python libararies. so i have know about this all along but haven't kn
 section_title2 = 'How to Create a Custom Module and Import It In the Python Repl'
 section_space2 = f'{section_title2}--------------------------------------------'
 print(section_space2)
+
+#notes
+"""
+Oh man was this a lesson. i came around in the end but man was it a rough start. so to start i should just say that all of this lesson was in the terminal and i do not have anything to paste 
+in here so i will have to try extra hard to explain what learned. so to start i would say if i have to deal with the terminal on my computer i should just use 'git bash'. if i put that in the 
+search bar it pops up. the commands are familiar and it just seems to work. once that is open you can move into the folder you want by typing 'cd name of folder' unlike some of the other terminals
+you dont need to put in the entire file path. once your in the syntax for making a new folder is 'mkdir name of directory' and the syntax to make a new file is 'touch name of file with.file type'. 
+then you can open up the file and work on it in what ever text editor you would like. once its saved you can go back to the terminal and type 'python name of file you want to run.with file type' and 
+it will run. now for the whole point of the video is importing custom modules but the thing is, if you make a file of reused fuctions, you can just import that and call the functions like it was any 
+other kind of libarary. That is so fucking cool. i can make helper files or my own personal modules and use them on any project that i could need them on. i will leave on, in the terminal if you want 
+to import something in python you would type python and it will tell you the version and will put you into a python terminal. from there type 'import name of file with no file type' and bam there you 
+go. I imagine that i could just import it in the terminal on here and call import in the file and it will probably work like all the other ones. man that is fucking cool. 
+"""
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+section_title3 = 'How to Import a Custom Python Module into Another File'
+section_space3 = f'{section_title3}--------------------------------------------'
+print(section_space3)
